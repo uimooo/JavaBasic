7d3b6369ab5bbcb0d842b4eb76e77fdb10d30a55
茂木悠里 <internationalmbany@gmail.com>
2018-03-20 15:19:00 +0900

    5-4

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -25,14 +25,35 @@ public class PTra05_04 {
 		// ①
 		System.out.println("0～6の整数を入力してください");
 
+
+
 		String line = scanner.nextLine();
 
+		while(line<6) {
 		switch(line) {
 			case "0":
 				System.out.println("日曜");
 				break;
 			case "1":
 				System.out.println("月曜");
+				break;
+			case "2":
+				System.out.println("火曜");
+				break;
+			case "3":
+				System.out.println("水曜");
+				break;
+			case "4":
+				System.out.println("木曜");
+				break;
+			case "5":
+				System.out.println("金曜");
+				break;
+			case "6":
+				System.out.println("土曜");
+			default:
+                System.out.println("0～6の整数を入力してください");
 		}
-	}
+		System.out.println("プログラムを終了します");
+	}}
 }
